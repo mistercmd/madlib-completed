5bec7dcbed3ac2f69fd78f8e24e2720f9257f431
mistercmd <carson.deru@gmail.com>
2018-07-18 08:35:11 -0600

    modified handleInputChange

--- src/components/card.js
+++ src/components/card.js
@@ -9,7 +9,7 @@ constructor() {
 
 
     this.state= {
-        color: 'BLUE',
+        color: '',
         pluralNoun: ''
     }
 
@@ -17,24 +17,18 @@ constructor() {
 }
 
     handleInputChange(event) {
-        this.setState({color: event.target.value})
+        this.setState({[event.target.name]: event.target.value})
+        console.log(this.state);
     }
 
 
    render() {
 
-    return (
-        <div className="card">
-            <h1>{this.state.color}</h1>
-            <Input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
-        </div>
-    )
       return (
          <div className="card">
             <h1>{this.state.color}</h1>
-            {Input('Color')}
-            {Input('Plural Noun')}
-
+            {Input('Color', this.state.color, this.handleInputChange, 'color')}
+            {Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun')}
          </div>
       )
    }
