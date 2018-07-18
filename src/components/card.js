dbe0eeee092bdb5f58010ba72b79fe0dce85ac7b
mistercmd <carson.deru@gmail.com>
2018-07-18 09:01:43 -0600

    added content visible state and generate and clear buttons with functionality.

--- src/components/card.js
+++ src/components/card.js
@@ -25,14 +25,21 @@ constructor() {
         adjectiveFour: '',
         nounThree: '',
         celebFour: '',
-        adjectiveFive: ''
+        adjectiveFive: '',
+        contentVisible: false
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
 }
 
     handleInputChange(event) {
         this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+        this.setState({contentVisible: !this.state.contentVisible})
     }
 
 
@@ -64,12 +71,17 @@ constructor() {
                 ]
 
       return (
-         <div className="card">
+         <form onSubmit={this.handleFormSubmit} className="card">
+            <div className="card__inputs">
             {
                 inputData.map(data => Input((data), this.handleInputChange))
             }
-            <Content data={this.state}/>
-         </div>
+            </div>
+            <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
+            {
+                this.state.contentVisible ? <Content data={this.state}/> :''
+            }
+            </form>
       )
    }
 }
