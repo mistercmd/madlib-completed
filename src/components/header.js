18f9959547e00e54c97d32b970bf52f8e7dea39d
mistercmd <carson.deru@gmail.com>
2018-07-19 10:36:19 -0600

    Finished styling, added text detecting color changer for number labels, began splitting main.scss into multiple files so it is easier to change.

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-        <div className="header__skew"></div>
-        <div className="header__border"></div>
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>Madlibs Carson Deru</h1>
             <p>Fill out the fields below and click the generate button to see the a madlib story.</p>
         </div>
